4766ad6ae4a7e21eb5700301c6e92a6bbad4576e
Jesse Rodriguez <wellpeng97@gmail.com>
2024-09-14 18:10:42 +0800

    fix: add Suspense for useSearchParams

--- src/app/client/auth0/callback/page.tsx
+++ src/app/client/auth0/callback/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/components/Button'
 import { useParams, useSearchParams } from 'next/navigation'
-import { Suspense, useCallback, useEffect, useMemo } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Text } from '@/components/text'
 import Image from 'next/image'
 import arexLogo from '@/images/logos/logo.png'
@@ -14,7 +14,7 @@ export default function ClientPage() {
   const { action } = useParams()
 
   const openAREX = useCallback(() => {
-    window.open(`arex://auth0/callback?code${params.get('code')}`)
+    window.open(`arex://auth0/callback?code={params.get('code')}`)
   }, [params])
 
   function handleCopyToken() {
@@ -27,24 +27,22 @@ export default function ClientPage() {
   }, [])
 
   return (
-    <Suspense fallback={<>Loading...</>}>
-      <div className="flex flex-col items-center justify-center p-8">
-        <Image
-          src={arexLogo}
-          alt="arex-logo"
-          width={128}
-          height={128}
-          className="m-4 drop-shadow-xl"
-        />
+    <div className="flex flex-col items-center justify-center p-8">
+      <Image
+        src={arexLogo}
+        alt="arex-logo"
+        width={128}
+        height={128}
+        className="m-4 drop-shadow-xl"
+      />
 
-        <Text className="p-4 font-semibold">Redirecting to AREX...</Text>
+      <Text className="p-4 font-semibold">Redirecting to AREX...</Text>
 
-        <Button onClick={openAREX}> Open AREX Client </Button>
+      <Button onClick={openAREX}> Open AREX Client </Button>
 
-        <Link href="#" onClick={handleCopyToken} className="mt-4">
-          Copy authorization token
-        </Link>
-      </div>
-    </Suspense>
+      <Link href="#" onClick={handleCopyToken} className="mt-4">
+        Copy authorization token
+      </Link>
+    </div>
   )
 }
