f440907a254d9bd73a1a0405ff1412a7779d6e42
Jesse Rodriguez <wellpeng97@gmail.com>
2024-09-23 20:10:45 +0800

    fix: console domain

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
-export const URL_AREX_CONSOLE = 'https://cloud.arextest.com/'
+export const URL_AREX_CONSOLE = 'https://console.arextest.com/'
 export const URL_AREX_DOC = 'https://docs.arextest.com/arexcloud/intro'
 export const URL_INTRO_VIDEO = 'https://www.youtube.com/watch?v=PrGjEx31clo'
